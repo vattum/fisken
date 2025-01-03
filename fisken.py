1bd0a3f50c1449183b88db2c342a12468731b0b9
Erik Sahakyan <erik.sahakyan@icloud.com>
2025-01-03 11:58:56 +0100

    lagt in spelkariaktär

--- fisken.py
+++ fisken.py
@@ -9,9 +9,35 @@ python -m arcade.examples.starting_template
 """
 import arcade
 
+SPRITE_SCALING = 0.15
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Fisken"
+
+MOVEMENT_SPEED = 5
+
+class Player(arcade.Sprite):
+    """ Player Class """
+
+    def update(self):
+        """ Move the player """
+        # Move player.
+        # Remove these lines if physics engine is moving player.
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check for out-of-bounds
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
 
 
 class MyGame(arcade.Window):
@@ -26,6 +52,12 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+        
         arcade.set_background_color(arcade.color.COAL)
 
         # If you have sprite lists, you should create them here,
@@ -34,7 +66,16 @@ class MyGame(arcade.Window):
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
         # Create your sprites and sprite lists here
-        pass
+        
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Player("bilder/fisk1.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
 
     def on_draw(self):
         """
@@ -45,7 +86,8 @@ class MyGame(arcade.Window):
         # the screen to the background color, and erase what we drew last frame.
         self.clear()
 
-        # Call draw() on all your sprite lists below
+        # Draw all the sprites.
+        self.player_list.draw()
 
     def on_update(self, delta_time):
         """
@@ -53,7 +95,8 @@ class MyGame(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        pass
+        # Move the player
+        self.player_list.update()
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -62,13 +105,30 @@ class MyGame(arcade.Window):
         For a full list of keys, see:
         https://api.arcade.academy/en/latest/arcade.key.html
         """
-        pass
+        
+        # If the player presses a key, update the speed
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
 
     def on_key_release(self, key, key_modifiers):
         """
         Called whenever the user lets off a previously pressed key.
         """
-        pass
+        # If a player releases a key, zero out the speed.
+        # This doesn't work well if multiple keys are pressed.
+        # Use 'better move by keyboard' example if you need to
+        # handle this.
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """
@@ -80,7 +140,7 @@ class MyGame(arcade.Window):
         """
         Called when the user presses a mouse button.
         """
-        pass
+        print(x, y, button)
 
     def on_mouse_release(self, x, y, button, key_modifiers):
         """
