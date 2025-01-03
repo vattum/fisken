ff229f6ecee4cb744c0db8415631e1b7256b06f8
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2025-01-03 12:21:49 +0100

    lägg till acceleration och friktion

--- fisken.py
+++ fisken.py
@@ -7,6 +7,7 @@ template.
 If Python and Arcade are installed, this example can be run from the command line with:
 python -m arcade.examples.starting_template
 """
+
 import arcade
 
 SPRITE_SCALING = 0.15
@@ -15,13 +16,21 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Fisken"
 
-MOVEMENT_SPEED = 5
+# Speed limit
+MAX_SPEED = 5.0
+
+# How fast we accelerate
+ACCELERATION_RATE = 0.2
+
+# How fast to slow down after we let off the key
+FRICTION = 0.05
+
 
 class Player(arcade.Sprite):
-    """ Player Class """
+    """Player Class"""
 
     def update(self):
-        """ Move the player """
+        """Move the player"""
         # Move player.
         # Remove these lines if physics engine is moving player.
         self.center_x += self.change_x
@@ -30,14 +39,17 @@ class Player(arcade.Sprite):
         # Check for out-of-bounds
         if self.left < 0:
             self.left = 0
+            self.change_x = 0  # Zero x speed
         elif self.right > SCREEN_WIDTH - 1:
             self.right = SCREEN_WIDTH - 1
+            self.change_x = 0
 
         if self.bottom < 0:
             self.bottom = 0
+            self.change_y = 0
         elif self.top > SCREEN_HEIGHT - 1:
             self.top = SCREEN_HEIGHT - 1
-
+            self.change_y = 0
 
 
 class MyGame(arcade.Window):
@@ -57,16 +69,22 @@ class MyGame(arcade.Window):
 
         # Set up the player info
         self.player_sprite = None
-        
+
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
         arcade.set_background_color(arcade.color.COAL)
 
         # If you have sprite lists, you should create them here,
         # and set them to None
 
     def setup(self):
-        """ Set up the game variables. Call to re-start the game. """
+        """Set up the game variables. Call to re-start the game."""
         # Create your sprites and sprite lists here
-        
+
         # Sprite lists
         self.player_list = arcade.SpriteList()
 
@@ -75,7 +93,6 @@ class MyGame(arcade.Window):
         self.player_sprite.center_x = SCREEN_WIDTH - 100
         self.player_sprite.center_y = SCREEN_HEIGHT / 2
         self.player_list.append(self.player_sprite)
-
 
     def on_draw(self):
         """
@@ -89,12 +106,54 @@ class MyGame(arcade.Window):
         # Draw all the sprites.
         self.player_list.draw()
 
+        # Display speed
+        arcade.draw_text(
+            f"X Speed: {self.player_sprite.change_x:6.3f}", 10, 50, arcade.color.BLACK
+        )
+        arcade.draw_text(
+            f"Y Speed: {self.player_sprite.change_y:6.3f}", 10, 70, arcade.color.BLACK
+        )
+
     def on_update(self, delta_time):
         """
         All the logic to move, and the game logic goes here.
         Normally, you'll call update() on the sprite lists that
         need it.
         """
+        # Add some friction
+        if self.player_sprite.change_x > FRICTION:
+            self.player_sprite.change_x -= FRICTION
+        elif self.player_sprite.change_x < -FRICTION:
+            self.player_sprite.change_x += FRICTION
+        else:
+            self.player_sprite.change_x = 0
+
+        if self.player_sprite.change_y > FRICTION:
+            self.player_sprite.change_y -= FRICTION
+        elif self.player_sprite.change_y < -FRICTION:
+            self.player_sprite.change_y += FRICTION
+        else:
+            self.player_sprite.change_y = 0
+
+        # Apply acceleration based on the keys pressed
+        if self.up_pressed and not self.down_pressed:
+            self.player_sprite.change_y += ACCELERATION_RATE
+        elif self.down_pressed and not self.up_pressed:
+            self.player_sprite.change_y += -ACCELERATION_RATE
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x += -ACCELERATION_RATE
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x += ACCELERATION_RATE
+
+        if self.player_sprite.change_x > MAX_SPEED:
+            self.player_sprite.change_x = MAX_SPEED
+        elif self.player_sprite.change_x < -MAX_SPEED:
+            self.player_sprite.change_x = -MAX_SPEED
+        if self.player_sprite.change_y > MAX_SPEED:
+            self.player_sprite.change_y = MAX_SPEED
+        elif self.player_sprite.change_y < -MAX_SPEED:
+            self.player_sprite.change_y = -MAX_SPEED
+
         # Move the player
         self.player_list.update()
 
@@ -105,30 +164,29 @@ class MyGame(arcade.Window):
         For a full list of keys, see:
         https://api.arcade.academy/en/latest/arcade.key.html
         """
-        
-        # If the player presses a key, update the speed
-        if key == arcade.key.UP:
-            self.player_sprite.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.player_sprite.change_y = -MOVEMENT_SPEED
-        #elif key == arcade.key.LEFT:
-        #    self.player_sprite.change_x = -MOVEMENT_SPEED
-        #elif key == arcade.key.RIGHT:
-        #    self.player_sprite.change_x = MOVEMENT_SPEED
 
+        if key == arcade.key.UP:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
 
     def on_key_release(self, key, key_modifiers):
         """
         Called whenever the user lets off a previously pressed key.
         """
-        # If a player releases a key, zero out the speed.
-        # This doesn't work well if multiple keys are pressed.
-        # Use 'better move by keyboard' example if you need to
-        # handle this.
-        if key == arcade.key.UP or key == arcade.key.DOWN:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player_sprite.change_x = 0
+
+        if key == arcade.key.UP:
+            self.up_pressed = False
+        elif key == arcade.key.DOWN:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """
@@ -150,7 +208,7 @@ class MyGame(arcade.Window):
 
 
 def main():
-    """ Main function """
+    """Main function"""
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
